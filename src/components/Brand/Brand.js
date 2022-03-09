3c330025b2419bed40e3c9d5f83b345ef5a43761
Paul Lockyer <plockyer@googlemail.com>
2022-03-09 14:39:19 +0000

    Added react router dom link for brand name to go home when clicked and styled link as if it was not styled as a anchor element

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Brand.css';
 
+import { Link } from 'react-router-dom';
+
 const Brand = (props) => {
-    return <div id="brand">{props.appName}</div>;
+    return <div id="brand"><Link to="/">{props.appName}</Link></div>;
 };
 
 export default Brand;
