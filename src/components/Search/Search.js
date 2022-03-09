b4a3923a2c6bae97cb4545ece704631dea4df058
Paul Lockyer <plockyer@googlemail.com>
2022-03-09 14:32:44 +0000

    Moved button onClick to its handleButtonClick function and removed the dispatch for performing the search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { performSearch, setSearchTerm } from '../../slices/Search/searchSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {  
+    setSearchTerm,
+    selectSearchTerms } from '../../slices/Search/searchSlice';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -11,30 +13,46 @@ const Search = () => {
 
     // alias the useDispatch method
     const dispatch = useDispatch();
+    let terms = useSelector(selectSearchTerms);
+   
+    // Handle the button click
+    const handleButtonClick = (e) => {
+
+        e.preventDefault();
+
+        // Save search term to localStorage
+        localStorage.setItem('searchTerms', terms);
+
+        // redirect to show the products found
+        window.location.href = '/products';
+
+    };
 
     // Display the search boxes
     return (
-        <form name="search" id="search" className="search-form">
+        <Form name="search" id="search" className="search-form">
 
             <Form.Control type="search"
             placeholder="Search"
             name="searchInput"
-            id="SearchInput" 
+            id="SearchInput"
+            value={terms ? terms : ''}
             onChange={(e) => {
                 dispatch(setSearchTerm(e.target.value));
             }}
-             />
+            />
 
+            
             <Button
             variant="warning"
             name="search"
             id="searchButton"
-            onClick={(e) => {
-                e.preventDefault();
-                dispatch(performSearch());
-            }}>Search</Button>
+            onClick={handleButtonClick}>
+                Search
+            </Button>
+            
 
-        </form>
+        </Form>
     );
 
 };
