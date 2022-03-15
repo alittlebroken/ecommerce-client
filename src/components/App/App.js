bd6a736795f503359ff723541601327bbec8dc93
Paul Lockyer <plockyer@googlemail.com>
2022-03-15 17:03:11 +0000

    Added responsive menu to the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,71 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 import './App.css';
 
 import Brand from '../Brand/Brand';
+import Search from '../Search/Search';
+import Navigation from '../Navigation/Navigation';
 
-function App() {
+const App = () => {
+
+  let authToken = JSON.parse(localStorage.getItem('token'));
+
+  // Handle hamburger click
+  const handleHamburgerClick = () => {
+
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".navmenu");
+
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+  }
+
   return (
-    <div id="app-container">
+    <div name="app-container">
+      
+      <header className="app-header">
 
-      <header id="app-header">
+        <nav className="navbar-grid">
 
-        <Brand />
+          <span
+          onClick={handleHamburgerClick}>
 
-        <Search />
+            <div className="hamburger">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
 
-        <ProfileIcon />
+          </span>
 
-        <Cart />
+          <Brand appName={process.env.REACT_APP_NAME}/>
+
+          <Search />
+
+          <Navigation authenticated={authToken} />
+
+        </nav>
 
       </header>
-      <Categories />
+
       <main id="app-main">
-      
+
+        <Routes>
+
+          <Route path="/"></Route>
+          <Route path="/products" element="Products"></Route>
+          <Route path="/product"></Route>
+          <Route path="/profile"></Route>
+          <Route path="/login"></Route>
+          <Route path="/logout"></Route>
+          <Route path="/register"></Route>
+          <Route path="/cart"></Route>
+
+        </Routes>
+
       </main>
+
     </div>
   );
 }
