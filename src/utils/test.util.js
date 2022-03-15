87a24af897be7d10d84a1213d8ed3e03b2d05a70
Paul Lockyer <plockyer@googlemail.com>
2022-03-15 17:00:35 +0000

    Overloaded the RTL render function to allow mu;tiple providers around the component being tested

---
+++ src/utils/test.util.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { render} from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+// Crester the intial state for the store
+const initialStateMock = {
+    results: [],
+    term: '',
+    category: 0,
+    isLoading: false,
+    hasError: false
+}
+
+// Mock out the store
+const storeMock = configureStore();
+
+// Create the store
+const store = storeMock(initialStateMock);
+
+// List all wrapped providers we need for our elements
+const AllProviders = ({children}) => {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                {children}
+            </BrowserRouter>
+        </Provider>
+    );
+};
+
+// Override the render fucntion with our own
+const customRender = (ui, options) => {
+    render(ui, {wrapper: AllProviders, ...options});
+}
+
+export * from '@testing-library/react';
+export {customRender as render};
