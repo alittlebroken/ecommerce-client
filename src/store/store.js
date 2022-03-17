017e53c5ed462a410edea00f9237853a902d876e
Paul Lockyer <plockyer@googlemail.com>
2022-03-17 16:39:06 +0000

    Added auth store reducer to configure store method

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // Import the various reducers
 import searchReducer from '../slices/Search/searchSlice';
+import authReducer from '../slices/Auth/authSlice';
 
 // Create and export the store
 const store = configureStore({
     reducer: {
-        search: searchReducer
+        search: searchReducer,
+        auth: authReducer
     }
 });
 
