9f2ab8bbfdb302e19db8d1717fc306ff812b5c20
Paul Lockyer <plockyer@googlemail.com>
2022-03-21 09:44:57 +0000

    Updated href for logout to redirect to login page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './Navigation.css';
+
+import { selectAuthenticated } from '../../slices/Auth/authSlice';
 
 const Navigation = (props) => {
 
     // extract the props
-    const { authenticated } = props;
+    const { authenticated, handleLogout } = props;
 
     return (
 
     <div className="navmenu">
         {!authenticated ? <a href="/login" className="navitem">Login</a> : <a href="/profile" className="navitem">Profile</a> }
         {!authenticated ?  <a href="/register" className="navitem">Register</a> : <a href="/cart" className="navitem">Cart</a> }
-        {!authenticated ?  '' : <a href="/logout" className="navitem">Logout</a> } 
+        {!authenticated ?  '' : <a href="/login" className="navitem" onClick={handleLogout}>Logout</a> } 
     </div>
 
     );
