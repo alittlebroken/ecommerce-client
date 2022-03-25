78c833c770847c0091aa521291962384bdc1daeb
Paul Lockyer <plockyer@googlemail.com>
2022-03-25 21:26:27 +0000

    added useEffect for loading products based on search criteria

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './ProductsList.css';
 import ProductCard from '../ProductCard/ProductCard';
 
-const ProductsList = (props) => {
+import { 
+    performSearch,
+    selectSearchResults,
+    selectSearchTerms
+  } from '../../slices/Search/searchSlice';
 
-    // Extract the passed in props
-    const { searchTerm, searchResults } = props;
+const ProductsList = () => {
+
+    // Alias the dispatch hook
+    const dispatch = useDispatch();
+
+    // Get search terms and serarch results
+    const searchTerm = useSelector(selectSearchTerms);
+    const searchResults = useSelector(selectSearchResults);
 
     // different text based on wether we have search terms to look for
     // First up when we have a search term
     const haveTerm = `Products matching ${searchTerm}`;
     // Now for when we have no term
     const haveNoTerm = `Latest products`;
+
+    // Get a list of products each time a new search term is set
+    useEffect(() => {
+
+        // Dispatch the perform search action
+        dispatch(performSearch({
+        searchTerms: searchTerm
+        }))
+
+    }, [dispatch, searchTerm]);
 
     // Render the component
     return (
