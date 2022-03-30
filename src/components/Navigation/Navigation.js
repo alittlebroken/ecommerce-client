c23c047552fa1e7831696732822e812dee55e97f
Paul Lockyer <plockyer@googlemail.com>
2022-03-30 11:46:19 +0100

    added getAuth to access authentication information. Added selectCartCount from cartSlice to indicate how many cart items someone has

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import './Navigation.css';
+import { getAuth } from '../../utils/auth';
+import { selectCartCount } from '../../slices/Cart/cartSlice';
 
-import { selectAuthenticated } from '../../slices/Auth/authSlice';
+import './Navigation.css';
 
 const Navigation = (props) => {
 
     // extract the props
-    const { authenticated, handleLogout } = props;
+    const { auth, token, user } = getAuth();
+    const { handleLogout } = props;
+
+    // Get the number of items in the cart if needed
+    const numCartItems = useSelector(selectCartCount);
 
     return (
 
     <div className="navmenu">
-        {!authenticated ? <a href="/login" className="navitem">Login</a> : <a href="/profile" className="navitem">Profile</a> }
-        {!authenticated ?  <a href="/register" className="navitem">Register</a> : <a href="/cart" className="navitem">Cart</a> }
-        {!authenticated ?  '' : <a href="/login" className="navitem" onClick={handleLogout}>Logout</a> } 
+        {!auth ? <a href="/login" className="navitem">Login</a> : <a href="/profile" className="navitem">Profile</a> }
+        {!auth ?  <a href="/register" className="navitem">Register</a> : <a href="/cart" className="navitem">Cart {numCartItems ? numCartItems : null}</a> }
+        {!auth ?  '' : <a href="/login" className="navitem" onClick={handleLogout}>Logout</a> } 
     </div>
 
     );
