9daba90033d6e0ec0501ba98aaaf4424b093e03e
Paul Lockyer <plockyer@googlemail.com>
2022-04-01 11:05:28 +0100

    Added CartItem component for every item in the cart and added function to update an items quantity

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,20 +4,48 @@ import { useSelector, useDispatch } from 'react-redux';
 import { 
     loadCart,
     selectCartCost,
-    selectCartItems
+    selectCartItems,
+    selectHasUpdated,
+    updateMyCart
 } from '../../slices/Cart/cartSlice';
 
 import { getAuth } from '../../utils/auth';
+
+import CartItem from '../CartItem/CartItem';
 
 import './Cart.css';
 
 const Cart = () => {
 
-    // Get the autnetication information
+    // Get the authentication information
     const { auth, user, token } = getAuth();
 
     // Alias the useDispatch hook
     const dispatch = useDispatch();
+
+    // Get the number of items in the cart
+    const currentCartCost = useSelector(selectCartCost);
+    const cartItems = useSelector(selectCartItems);
+    const cartUpdate = useSelector(selectHasUpdated);
+
+     // Handle the decrease count click
+     const updateQuantity = (product_id, amount) => {
+       
+        let itemQuantity = amount;
+
+        if(itemQuantity < 0){
+            itemQuantity = 0;
+        }
+
+        const payload = {
+            token,
+            cart_id: user.cart, 
+            product_id: product_id, 
+            quantity: itemQuantity
+        };
+
+        dispatch(updateMyCart(payload));
+    };
 
     // Update the state with the cart contents for the logged in user
     useEffect(() => {
@@ -25,19 +53,22 @@ const Cart = () => {
             cart_id: user.cart,
             token: token
         }));
-    }, [dispatch, token, user.cart]);
-
-    // Get the number of items in the cart
-    const currentCartCost = useSelector(selectCartCost);
-    const cartItems = useSelector(selectCartItems);
+    }, [dispatch, token, user.cart, cartUpdate]);
 
     return (
 
         <div role="presentation" className="cart-container">
 
             <h3>Cart for {user.email}</h3>
-            <h5>Current cart total £{currentCartCost}</h5>
+            <h5>Current cart total £{parseFloat(currentCartCost).toFixed(2)}</h5>
             
+            {cartItems.map(cartItem => {
+                return <CartItem 
+                key={cartItem.product_id} 
+                item={cartItem} 
+                onHandleClick={updateQuantity}/>
+            })}
+
             <button className="button">Place order</button>
         </div>
     )
