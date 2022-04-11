b23705d9ca74948b42854947d8d3638cc1aeb696
Paul Lockyer <plockyer@googlemail.com>
2022-04-11 21:46:30 +0100

    api method created to get customer orders from API

---
+++ src/api/orders/orders.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.REACT_APP_API_URL;
+
+/**
+ * Function to get all of a customers orders
+ */
+export const getCustomerOrders = async (payload) => {
+
+    /**
+     * Contact the API and get the data
+     */
+    try{
+
+        const response = await axios.get(`${BASE_URL}/users/${payload.user_id}/orders?secret_token=${payload.token}`)
+        console.log(response);
+        return response;
+
+    } catch(error) {
+        throw error;
+    }
+
+};
