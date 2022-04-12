0f3e68019f5745ab165bf20edbce93911d960a11
Paul Lockyer <plockyer@googlemail.com>
2022-04-12 15:54:12 +0100

    thunk added for loading orders

--- src/slices/Orders/ordersSlice.js
+++ src/slices/Orders/ordersSlice.js
@@ -1,6 +1,34 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 /**
+ * Import the API functions
+ */
+import { getCustomerOrders } from '../../api/orders/orders';
+
+/**
+ * Thunk for loading the orders data into the store
+ */
+export const loadOrders = createAsyncThunk(
+    'orders/loadOrders',
+    async (payload, thunkAPI) => {
+
+        /**
+         * get the orders for this customer from the API
+         */
+        try{
+
+            const response = await getCustomerOrders(payload);
+            console.log(response)
+            return JSON.stringify(response);
+
+        } catch(error) {
+            throw error;
+        }
+
+    }
+);
+
+/**
  * Set the initial state for the store
  */
 const initialState = {
@@ -19,7 +47,48 @@ const ordersSlice = createSlice({
     name: 'orders',
     initialState: initialState,
     reducers: {},
-    extraReducers: {},
+    extraReducers: {
+        [loadOrders.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadOrders.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        },
+        [loadOrders.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = false;
+
+            /**
+             * parse the JSON response
+             */
+            const result = JSON.parse(action.payload)
+            
+            /**
+             * Load the data into the state
+             */
+            state.orders = result.data;
+
+            /**
+             * Set the total count of orders
+             */
+            state.total = state.orders.length;
+
+            /**
+             * Set the pending orders
+             */
+            const pending = state.orders.filter(item => item.order_arrived == null);
+            state.pending = pending.length;
+
+            /**
+             * Set the completed orders
+             */
+            const completed = state.orders.filter(item => item.order_arrived != null);
+            state.completed = completed.length;
+
+        }
+    },
 });
 
 /**
