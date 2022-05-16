808e3681139abd6f36f316b9590237282c45db92
Paul Lockyer <plockyer@googlemail.com>
2022-05-16 15:03:49 +0100

    added getting any applicable messages displayed

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -4,6 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import './ProductsList.css';
 import ProductCard from '../ProductCard/ProductCard';
 
+import {
+    selectMessage,
+    selectType,
+    selectCode
+} from '../../slices/Messages/messagesSlice';
+
 import { 
     performSearch,
     selectSearchResults,
@@ -18,6 +24,26 @@ const ProductsList = () => {
     // Get search terms and serarch results
     const searchTerm = useSelector(selectSearchTerms);
     const searchResults = useSelector(selectSearchResults);
+
+    /**
+     * Gather data from the slices
+     */
+     const message = useSelector(selectMessage);
+     const messageType = useSelector(selectType);
+     const messageCode = useSelector(selectCode);
+     let messageStyle = '';
+ 
+     /**
+      * Check the type of message and set appropriate
+      * styles ect
+      */
+     if(messageType === 'OK'){
+         messageStyle = 'message-ok';
+     } else if (messageType === 'WARNING') {
+         messageStyle = 'message-warn';
+     } else if (messageType === 'ERROR') {
+         messageStyle = 'message-error';
+     }
 
     // different text based on wether we have search terms to look for
     // First up when we have a search term
@@ -48,6 +74,10 @@ const ProductsList = () => {
                     return <ProductCard key={product.product_id} data={product} />;
                 })}
 
+            </div>
+
+            <div role="presentation" className={`messages ${messageStyle}`} >
+                {message ? message : null}
             </div>
 
         </div>
