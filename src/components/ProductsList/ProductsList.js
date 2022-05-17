2345d6f20d13747006215361dcbe1ab1ef292631
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 11:48:43 +0100

    removed old message code and added new messages component

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -4,17 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import './ProductsList.css';
 import ProductCard from '../ProductCard/ProductCard';
 
-import {
-    selectMessage,
-    selectType,
-    selectCode
-} from '../../slices/Messages/messagesSlice';
-
 import { 
     performSearch,
     selectSearchResults,
     selectSearchTerms
   } from '../../slices/Search/searchSlice';
+
+import Messages from '../Messages/Messages';
 
 const ProductsList = () => {
 
@@ -24,26 +20,6 @@ const ProductsList = () => {
     // Get search terms and serarch results
     const searchTerm = useSelector(selectSearchTerms);
     const searchResults = useSelector(selectSearchResults);
-
-    /**
-     * Gather data from the slices
-     */
-     const message = useSelector(selectMessage);
-     const messageType = useSelector(selectType);
-     const messageCode = useSelector(selectCode);
-     let messageStyle = '';
- 
-     /**
-      * Check the type of message and set appropriate
-      * styles ect
-      */
-     if(messageType === 'OK'){
-         messageStyle = 'message-ok';
-     } else if (messageType === 'WARNING') {
-         messageStyle = 'message-warn';
-     } else if (messageType === 'ERROR') {
-         messageStyle = 'message-error';
-     }
 
     // different text based on wether we have search terms to look for
     // First up when we have a search term
@@ -76,9 +52,7 @@ const ProductsList = () => {
 
             </div>
 
-            <div role="presentation" className={`messages ${messageStyle}`} >
-                {message ? message : null}
-            </div>
+            <Messages />
 
         </div>
         </div>
