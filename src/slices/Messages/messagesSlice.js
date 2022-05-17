2092d81c35ddbbce789b55d272eaf844094e2b62
Paul Lockyer <plockyer@googlemail.com>
2022-05-17 10:44:12 +0100

    added ability to have a list of messages

--- src/slices/Messages/messagesSlice.js
+++ src/slices/Messages/messagesSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import * as uuid from 'uuid';
 
 /**
  * Initial state for the store
  */
 const initialState = {
+    messages: [],
     content: '',
     type: '',
     code: '',
@@ -24,6 +26,12 @@ const messageSlice = createSlice({
         },
         setCode: (state, action) => {
             state.code = action.payload;
+        },
+        removeMessage: (state, action) => {
+            /**
+             * Filter out any message which has the same message ID
+             */
+            state.messages = state.messages.filter(message => message.id !== action.payload);
         }
     },
     extraReducers: {
@@ -40,6 +48,14 @@ const messageSlice = createSlice({
              * Generate a message based on the payload
              */
             if(payload.status === 201 && payload.statusText === 'Created'){
+                
+                state.messages.push({
+                    id: uuid.v4(),
+                    content: 'Item has been added to the cart',
+                    type: 'OK',
+                    code: 201,
+                })
+
                 state.content = "Item added to cart";
                 state.type = "OK";
                 state.code = 201;
@@ -58,6 +74,14 @@ const messageSlice = createSlice({
             /**
              * Generate a message based on the payload
              */
+
+            state.messages.push({
+                id: uuid.v4(),
+                content: 'Unable to add item to the cart',
+                type: 'ERROR',
+                code: 400,
+            })
+
             state.content = "There was an issue adding the desired item to the cart";
             state.type = "ERROR";
             state.code = "400";
@@ -70,13 +94,14 @@ const messageSlice = createSlice({
  * Export selects for the slices data
  */
 export const selectMessage = state => state.messages.content;
+export const selectMessages = state => state.messages;
 export const selectType = state => state.messages.type;
 export const selectCode = state => state.messages.code;
 
 /**
  * Action exports
  */
-export const { setMessage, setType, setCode } = messageSlice.actions;
+export const { setMessage, setType, setCode, removeMessage } = messageSlice.actions;
 
 /**
  * Default reducer export
